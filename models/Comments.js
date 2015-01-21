f55076f7c23b78d75a9f185b1ef7eff16b582a9b
Ghazni <ghazni.nattarshah@objectfrontier.com>
2015-01-21 19:01:34 +0530

    Initial version

---
+++ models/Comments.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+
+var CommentSchema = new mongoose.Schema({
+
+    body: String, 
+    author: String,
+    upvotes: {type: Number, default: 0},
+    post: {type: mongoose.Schema.Types.ObjectId, ref: 'post'}
+});
+
+mongoose.model('Comment', CommentSchema);
---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose');
+
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
+
+module.exports = function(app) {
+
+    app.route('/posts')
+    .get(function(req, res, next) {
+        Post.find(function(err, posts) {
+
+            if (err) { return next(err); }
+            res.json(posts);
+        });
+    })
+    .post(function(req, res, next){
+
+        var post = new Post(req.body);
+        post.save(function(err, post) {
+
+            if (err) { return next(err); }
+            res.json(post);
+        });
+    });
+};
